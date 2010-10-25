5dc83a359ad12cbe06d7f0ce2354f90ff34c7172
roberto.guida <roberto.guida@gmail.com>
2010-10-25 17:24:29 +0000

    FIX - constant MAX_TYPES declaration.

--- Bolle Level Editor/Bolle Level Editor/bolla.h
+++ Bolle Level Editor/Bolle Level Editor/bolla.h
@@ -1,15 +1,17 @@
 #pragma once
 
-#define TYPES_MAX 8;
+int const TYPES_MAX = 8;
 
 class bolla
 {
 	int x;
 	int y;
 	int type;
+
 public:
 	bolla(void);
 	~bolla(void);
+
 	int getx();
 	void putx(int newx);
 	int gety();
